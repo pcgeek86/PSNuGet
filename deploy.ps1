9e31fc9dc99384d4ea6ecdd4bf60b9c005346010
Trevor Sullivan <trevor@trevorsullivan.net>
2015-07-23 15:42:23 -0600

    Updated deploy.ps1

--- deploy.ps1
+++ deploy.ps1
@@ -8,6 +8,7 @@
 	This PowerShell script is invoked by AppVeyor after the "Deploy" phase has completed successfully.
 #>
 
+Find-Package -ForceBootstrap -Name z*;
 Import-Module -Name $env:APPVEYOR_BUILD_FOLDER;
-
+Get-Module;
 Publish-Module -Name PoshNuGet -NuGetApiKey $env:psapikey;
