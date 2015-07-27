0d6279dbad041c94e81b412b78d1848406e45027
Trevor Sullivan <trevor@trevorsullivan.net>
2015-07-27 11:14:01 -0600

    release test

--- deploy.ps1
+++ deploy.ps1
@@ -12,6 +12,7 @@ $VerbosePreference = 'continue';
 
 
 ### Skip the build if the Git tag does not match "release"
+Write-Output -InputObject "Git tag is: $env:APPVEYOR_REPO_TAG_NAME";
 if ($env:APPVEYOR_REPO_TAG_NAME -notmatch 'release') {
 	throw 'The release tag was not found on this commit. Skipping deployment.';
 	return;
