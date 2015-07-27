8871ea889aad91c8c7e73b5a4fee6c3cb52e4a5a
Trevor Sullivan <trevor@trevorsullivan.net>
2015-07-27 11:00:37 -0600

    Release test

--- deploy.ps1
+++ deploy.ps1
@@ -9,6 +9,16 @@
 #>
 
 $VerbosePreference = 'continue';
+
+
+### Skip the build if the Git tag does not match "release"
+if ($env:APPVEYOR_REPO_TAG_NAME -notmatch 'release') {
+	throw 'The release tag was not found on this commit. Skipping deployment.';
+	return;
+} else {
+		Write-Verbose -Message ('The release tag ({0}) was found on this commit. Starting deployment.' -f $env:APPVEYOR_REPO_TAG_NAME);
+}
+
 'AppVeyor Build Folder: {0}' -f $env:APPVEYOR_BUILD_FOLDER;
 Write-Verbose -Message 'Calling Find-Package command to download nuget-anycpu.exe'
 Find-Package -ForceBootstrap -Name zzzzzz -ErrorAction Ignore;
