dedce0525fb9dcc1501c41c0a5569427a6f6c201
Trevor Sullivan <trevor@trevorsullivan.net>
2015-07-28 11:03:51 -0600

    Update deploy.ps1

--- deploy.ps1
+++ deploy.ps1
@@ -36,8 +36,8 @@ function Main {
 	### Skip the build if the Git tag does not match "release"
 	Write-Output -InputObject "Git tag is: $env:APPVEYOR_REPO_TAG_NAME";
 	if ($env:APPVEYOR_REPO_TAG_NAME -notmatch 'release') {
-		throw 'The release tag was not found on this commit. Skipping deployment.';
-		return;
+#		throw 'The release tag was not found on this commit. Skipping deployment.';
+#		return;
 	} else {
 			Write-Verbose -Message ('The release tag ({0}) was found on this commit. Starting deployment.' -f $env:APPVEYOR_REPO_TAG_NAME);
 	}
